5f7d60cab793851c11c862aa8ff3520571e3a2cb
Parvin Farahzadeh <parv93farah@gmail.com>
2022-04-19 16:20:22 -0400

    Implement socket IO on server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var createError = require('http-errors');
 var express = require('express');
+const http = require('http');
+const socketio = require('socket.io'); 
+const cors = require('cors');
+const moment = require('moment'); 
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
 
@@ -10,6 +12,29 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//create socket io server 
+var server = http.createServer(app);
+const io = socketio(server, {
+  cors: true, 
+  origin: 'https://localhost:3001',
+});
+
+
+io.on("connect", (socket) => {
+  console.log(`${socket.id} connected!`);
+  //TODO: assign a player and a playerId as they register. 
+  // socket.join('game-room');
+
+  socket.on('disconnect', function() {
+    console.log(`${socket.id} disconnected!`)
+  })
+});
+
+// setInterval(()=>{
+//   io.to('clock-room').emit('time', new Date())
+// },1000)
+
 
 // view engine setup
 if (process.env.NODE_ENV === 'production' || process.env.PREVIEW === 'true'){
@@ -32,6 +57,11 @@ app.get('/*', function(req, res) {
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, function() {
-console.log(`Express app running on port ${port}`)
-});
+// app.listen(port, function() {
+// console.log(`Express app running on port ${port}`)
+// });
+
+server.listen(port, err=> {
+  if(err) console.log(err)
+  console.log('Server running on Port', port)
+})
